b0f89162675eed0f6e52a62e49b97e12e4beae66
zkl <1090890128@qq.com>
2021-01-06 16:00:02 +0800

    获取文件信息 断点续传 兼容apple

--- Xl.Core.API/Controllers/UserController.cs
+++ Xl.Core.API/Controllers/UserController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Xl.Core.API.Filters;
 using Xl.Core.ArchiveService.User;
+using Xl.Core.DomainModel.File;
 using Xl.Core.UniversalCommon.Log;
 
 namespace Xl.Core.API.Controllers
@@ -22,7 +24,7 @@ namespace Xl.Core.API.Controllers
             this.userService = service;
         }
 
-        [LogTrace]
+        //[LogTrace]
         public ActionResult Login(int id)
         {
             string msg = "登录时间:" + DateTime.Now.ToString();
@@ -30,5 +32,67 @@ namespace Xl.Core.API.Controllers
             var userInfo = userService.GetAllInfoById(id);
             return Ok(userInfo);
         }
+
+        /// <summary>
+        /// 获取文件 兼容apple
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
+        public FileResult GetFilesById(long fileId)
+        {
+            //var file = _fileService.GetFileInfoDetail(fileId);
+            //获取文件信息
+            var file = new AttachFile();
+            var bytes = System.IO.File.ReadAllBytes(file.LocalPath);
+            string contentType = "";
+            new FileExtensionContentTypeProvider().TryGetContentType(file.LocalPath, out contentType);
+
+            var context = ControllerContext.HttpContext;
+            var reqRange = context.Request.Headers["Range"].ToString();
+            string[] reqBlockRange = null;
+            if (!string.IsNullOrEmpty(reqRange))
+            {
+                reqBlockRange = reqRange.Replace("bytes=", "").Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                context.Response.StatusCode = 206;
+
+                long fileSize = bytes.Length;
+                long startPosition = 0;
+                long partialSize = fileSize;
+                if (reqBlockRange != null)
+                {
+                    startPosition = Convert.ToInt64(reqBlockRange[0]);
+                    if (reqBlockRange.Length > 1)
+                    {
+                        long endPosition = fileSize;
+                        if (long.TryParse(reqBlockRange[1], out endPosition))
+                        {
+                            partialSize = endPosition - startPosition + 1;
+                        }
+                    }
+                    else
+                    {
+                        partialSize = fileSize - startPosition;
+                    }
+                }
+
+                byte[] buffer = new byte[(int)partialSize];
+                Array.Copy(bytes, startPosition, buffer, 0, partialSize);
+
+                context.Response.Headers.Add("accept-ranges", "bytes");
+                context.Response.Headers.Add("access-control-allow-methods", "HEAD, GET, OPTIONS");
+                context.Response.Headers.Add("cache-control", "public, max-age=30726563");
+                context.Response.ContentType = contentType;
+                context.Response.Headers.Add("Connection", "keep-alive");
+                context.Response.Headers.Add("content-range", $"bytes {startPosition}-{startPosition + partialSize - 1 }/{fileSize}");
+                context.Response.Headers.Add("Content-Length", $"{partialSize}");
+                return File(buffer, contentType);
+            }
+            else
+            {
+                return File(bytes, contentType);
+            }
+        }
+
+
     }
 }
---
+++ Xl.Core.DomainModel/File/AttachFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xl.Core.DomainModel.File
+{
+    /// <summary>
+    /// 文件附件 
+    ///</summary>
+    public class AttachFile
+    {
+        /// <summary>
+        /// 唯一标识
+        /// <summary>
+        public long FileId { get; set; }
+        /// <summary>
+        /// 原始文件名称
+        /// <summary>
+        public string FileName { get; set; }
+        /// <summary>
+        /// 文件后缀名
+        /// <summary>
+        public string Suffix { get; set; }
+        /// <summary>
+        /// 文件一级分类代码
+        /// <summary>
+        public string ClassCode1 { get; set; }
+        /// <summary>
+        /// 文件二级分类代码
+        /// <summary>
+        public string ClassCode2 { get; set; }
+        /// <summary>
+        /// 文件大小  (单位：字节)
+        /// <summary>
+        public long FileSize { get; set; }
+        /// <summary>
+        /// 本地路径  格式为"一级分类代码/二级分类代码/上传日期/fileId.文件后缀名",如"ep/arc/20171127/6187982494351360.docx"
+        /// <summary>
+        public string LocalPath { get; set; }
+        /// <summary>
+        /// 服务器虚拟路径
+        /// <summary>
+        public string VirtualPath { get; set; }
+        /// <summary>
+        /// 上传时间
+        /// <summary>
+        public long UploadTime { get; set; }
+        /// <summary>
+        /// 文件说明
+        /// <summary>
+        public string Remark { get; set; }
+        /// <summary>
+        /// 文件类型   1 副文本文件  0  下载文件
+        /// <summary>
+        public int Type { get; set; }
+    }
+}
